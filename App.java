2e8ef07fc119c444afa4a27728022780299faf88
tkos94 <43825032+tkos94@users.noreply.github.com>
2023-07-17 00:19:57 -0300

    #1 commit

---
+++ App.java
@@ -0,0 +1,12 @@
+package tech.ada.java;
+
+public class App {
+
+    public static void main(String[] args) {
+        // Criar uma instância da classe AppController.
+        VideoGameSalesController videoGameSalesController = new VideoGameSalesController();
+
+        // Executar o aplicativo.
+        videoGameSalesController.run();
+    }
+}
